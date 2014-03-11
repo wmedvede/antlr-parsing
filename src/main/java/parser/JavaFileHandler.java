74f960b0c6f19d807dbb9bcdf46c7c1d871b9055
Walter Medvedeo <wmedvedeo@gmail.com>
2014-03-11 15:10:18 +0100

    JavaFieldHandler1Test initial tests works

--- src/main/java/parser/JavaFileHandler.java
+++ src/main/java/parser/JavaFileHandler.java
@@ -1,196 +1,42 @@
 package parser;
 
-import parser.descr.*;
-import parser.util.ParserUtil;
+import parser.descr.FileDescr;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public interface JavaFileHandler {
 
-public class JavaFileHandler {
+    FileDescr getFileDescr();
 
-    private StringBuilder source = null;
+    String getOriginalContent();
 
-    private JavaParser parser;
+    String buildResult();
 
-    private FileDescr fileDescr;
+    void createImport(String source);
 
-    private Map<ElementDescriptor, ElementDescriptor> deletedElements = new HashMap<ElementDescriptor, ElementDescriptor>();
+    void removePackageImport(String packageName);
 
-    private Map<ElementDescriptor, ElementDescriptor> originalElements = new HashMap<ElementDescriptor, ElementDescriptor>();
+    void removeClassImport(String className);
 
-    private List<ElementDescriptor> allElements = new ArrayList<ElementDescriptor>();
+    void setPackageName(String name);
 
-    public JavaFileHandler(InputStream inputStream) throws Exception {
-        //TODO implement better exceptions handling
-        source = new StringBuilder(ParserUtil.readString(inputStream));
-        parseSource();
-    }
+    void setClassName(String name);
 
-    public void deleteField(String name) throws Exception {
-        if (fileDescr.getClassDescr() != null) {
-            for (FieldDescr field : fileDescr.getClassDescr().getFields()) {
-                int deletedDeclarations = 0;
-                for (VariableDeclarationDescr var : field.getVariableDeclarations()) {
-                    if (var.getIdentifier().equals(name)) {
-                        deleteElement(var);
-                    }
-                    if (deletedElements.containsKey(var)) {
-                        deletedDeclarations++;
-                    }
-                }
-                if (deletedDeclarations == field.getVariableDeclarations().size()) deleteElement(field);
-            }
-        }
-    }
+    void setSuperClassName(String className);
 
-    public void deleteMethod(String name) throws Exception {
-        //TODO implement overloading, constructors deletion, etc.
-        if (fileDescr.getClassDescr() != null) {
-            for (MethodDescr method : fileDescr.getClassDescr().getMethods()) {
-                if (name.equals(method.getName())) {
-                    deleteElement(method);
-                }
-            }
-        }
-    }
+    void createClassAnnotation(String source);
 
-    public void addField(String sentences) {
-        ElementDescriptor lastField = null;
-        for (ElementDescriptor member : allElements) {
-            if (member.getElementType() == ElementDescriptor.ElementType.FIELD) lastField = member;
-        }
-        addMemberAfter(lastField, ElementDescriptor.ElementType.FIELD, sentences);
-    }
+    void deleteClassAnnotation(String annotationClassName);
 
-    public void addMethod(String sentences) {
-        ElementDescriptor lastMethod = null;
-        for (ElementDescriptor member : allElements) {
-            if (member.getElementType() == ElementDescriptor.ElementType.METHOD) lastMethod = member;
-        }
-        addMemberAfter(lastMethod, ElementDescriptor.ElementType.METHOD, sentences);
-    }
+    void createField(String source);
 
-    public void addMember(String sentences) {
-        allElements.add(allElements.size(), new ElementDescriptor(ElementDescriptor.ElementType.SENTENCE, sentences, -1, -1));
-    }
+    void renameField(String name, String newName);
 
-    public String getOriginalContent() {
-        return source.toString();
-    }
+    void deleteField(String name);
 
-    public String build() {
+    void createFieldAnnotation(String fieldName, String source);
 
-        StringBuilder dest = new StringBuilder(source.length());
-        int sourceIndex = 0;
+    void deleteFieldAnnotation(String fieldName, String annotationClassName);
 
-        for (ElementDescriptor member : allElements) {
+    void createMethod(String source);
 
-            if (isOriginal(member)) {
-                if (sourceIndex < member.getStart()) {
-                    dest.append(source.substring(sourceIndex, member.getStart()));
-                }
-                if (!isDeleted(member)) {
-                    copyOriginalElement(member, dest);
-                }
-                sourceIndex = member.getStop() + 1;
-            } else if (!isDeleted(member)) {
-                dest.append(member.getText());
-            }
-        }
-        if (sourceIndex < source.length()) {
-            dest.append(source.substring(sourceIndex, source.length()));
-        }
-
-        return dest.toString();
-    }
-
-    private void parseSource() throws Exception {
-        parser = ParserUtil.initParser(source.toString());
-        parser.compilationUnit();
-        fileDescr = parser.getFileDescr();
-        if (fileDescr.getClassDescr() != null) {
-            for (ElementDescriptor member : fileDescr.getClassDescr().getMembers()) {
-                originalElements.put(member, member);
-                allElements.add(member);
-            }
-        }
-    }
-
-    private void deleteElement(ElementDescriptor element) throws Exception {
-        deletedElements.put(element, element);
-    }
-
-    private void copyOriginalElement(ElementDescriptor element, StringBuilder dest) {
-        switch (element.getElementType()) {
-            case FIELD:
-                copyField((FieldDescr)element, dest);
-                break;
-            default:
-                dest.append(source.substring(element.getStart(), element.getStop()+1));
-        }
-    }
-
-    private void copyField(FieldDescr field, StringBuilder dest) {
-        //TODO this method should be improved when we add annotations processing, etc.
-        StringBuilder tmpBuilder = new StringBuilder( field.getStop() - field.getStart() );
-        tmpBuilder.append(source.substring(field.getModifiers().getStart(), field.getModifiers().getStop()+1));
-        int sourceIndex = field.getModifiers().getStop() + 1;
-        boolean previousDeleted = false;
-
-        for (VariableDeclarationDescr varDec : field.getVariableDeclarations()) {
-            if (sourceIndex < varDec.getStart()) {
-                tmpBuilder.append(source.substring(sourceIndex, varDec.getStart()));
-            }
-            if (previousDeleted) {
-                //TODO this deletion should be improved.
-                int index = tmpBuilder.lastIndexOf(",");
-                if (index > 0) {
-                    tmpBuilder.deleteCharAt(index);
-                }
-            }
-            if (!isDeleted(varDec)) {
-                tmpBuilder.append(source.substring(varDec.getStart(), varDec.getStop()+1));
-                previousDeleted = false;
-            } else {
-                //check if a "," for needs to be deleted
-                previousDeleted = true;
-            }
-            sourceIndex = varDec.getStop() +1;
-        }
-        if (previousDeleted) {
-            //TODO this deletion should be improved.
-            int index = tmpBuilder.lastIndexOf(",");
-            if (index > 0) {
-                tmpBuilder.deleteCharAt(index);
-            }
-        }
-        if (sourceIndex <= field.getStop()) {
-            tmpBuilder.append(source.substring(sourceIndex, field.getStop() + 1));
-        }
-        dest.append(tmpBuilder.toString());
-    }
-
-    private boolean isDeleted(ElementDescriptor element) {
-        return deletedElements.containsKey(element);
-    }
-
-    private boolean isOriginal(ElementDescriptor element) {
-        return originalElements.containsKey(element);
-    }
-
-    private void addMemberAfter(ElementDescriptor member, ElementDescriptor.ElementType type, String sentences) {
-        int index = member != null ?  allElements.indexOf(member) : -1;
-        index = index < 0 ? allElements.size() : (index+1);
-        allElements.add(index, new ElementDescriptor(type, sentences, -1, -1));
-    }
-
-    private void addMemberBefore(ElementDescriptor member, ElementDescriptor.ElementType type, String sentences) {
-        int index = member != null ?  allElements.indexOf(member) : -1;
-        index = index < 0 ? 0 : (index+1);
-        allElements.add(index, new ElementDescriptor(type, sentences, -1, -1));
-    }
-
+    void deleteMethod(String name, String[] paramTypes);
 }
